479f8dd8bd7a375ba03622b21953c321ede8bc91
Milya <xmilya1694@gmail.com>
2023-05-02 19:43:01 +0300

    state

--- src/components/AddList/index.js
+++ src/components/AddList/index.js
@@ -1,14 +1,17 @@
 import React, {useState} from 'react';
 import List from '../List';
 import './AddList.scss';
+import Badge from '../Badge';
 
-import plusSvg from '../../assets/img/plus-black.svg'
-
+import plusSvg from '../../assets/img/plus-black.svg';
+import closeSvg from '../../assets/img/close.svg';
 
 
 const AddList = ({colors}) => {
 
     const [visiblePopup, setVisiblePopup] = useState(true);
+    const [selectedColor, selectColor] = useState(colors[0].id);
+   
 
     return (
         <div className="add-list">
@@ -22,12 +25,21 @@ const AddList = ({colors}) => {
                 },  
             ]} />
             {visiblePopup && (<div className="add-list__popup">
+                <img 
+                onClick={() => setVisiblePopup(false)}
+                src={closeSvg} 
+                alt="Close button" 
+                className='add-list__popup-close-btn' 
+                />
                 <input className="field" type="text" placeholder="Название списка"/>
-                <div className="add-list__pop-colors">
-                <ul>
-                    <li></li>
-                    <li></li>
-                </ul>
+                <div className="add-list__popup-colors">
+                    {colors.map(color => <Badge 
+                            onClick={() => selectColor(color.id)} 
+                            key={color.id} 
+                            color={color.name}
+                            className={selectedColor === color.id && 'active'}
+                    />)
+                    }
                 </div>
                 <button className='button'>Добавить</button>
             </div>)}
